89585b04397aecd0441bbebab8388b42543bf779
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 04:30:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-div = num1 / num2
-return div
+  div = num1 / num2
+  return div
 end
 
 def multiplication(num1, num2)
-mult = num1 * num2
-return mult
+  mult = num1 * num2
+  return mult
 end
 
 def modulo(num1, num2)
-
+  mod = num2 % num1
+  return mod
 end
 
 def square_root(num)
